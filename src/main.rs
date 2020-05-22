8a0f34a68fac6b0ca6d071b4cecf9fb18a2d8423
Nirmoy Das <nirmoy.aiemd@gmail.com>
2020-05-22 09:07:09 +0200

    stop watson before quit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use swayipc::{Connection, EventType, Fallible};
 use swayipc::reply::Event;
 use swayipc::reply::WindowChange;
 use std::process::Command;
+use std::process;
+
+
+use std::{error::Error, thread};
+use signal_hook::{iterator::Signals, SIGTERM, SIGINT};
 
 
 
@@ -10,6 +15,17 @@ async fn main() -> Fallible<()> {
     let subs = [
         EventType::Window,
     ];
+    let signals = Signals::new(&[SIGTERM, SIGINT])?;
+
+        thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("stoping watson {:?}\n", sig);
+            Command::new("watson").arg("stop").status().expect("");
+            process::exit(0);
+        }
+    });
+
+
     let mut events = Connection::new().await?.subscribe(&subs).await?;
     while let event = events.next().await {
         if let Event::Window(window) = event.unwrap() {
